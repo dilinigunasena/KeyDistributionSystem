07944aa1688bf0a4ec5bfcba7a73d3211370d4e0
jkehelwala <git@jkehelwala.com>
2018-10-04 14:11:02 +0530

    Database updates

--- db/privileges.sql
+++ db/privileges.sql
@@ -4,3 +4,5 @@ GRANT SELECT ON keydist.accounts TO 'basic'@'%' ;
 CREATE USER 'machine'@'%' IDENTIFIED BY 'machine';
 GRANT SELECT ON keydist.accounts TO 'machine'@'%' ;
 GRANT SELECT, INSERT ON keydist.requests TO 'machine'@'%' ;
+
+-- Todo
